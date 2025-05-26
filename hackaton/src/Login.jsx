33ce2f9c5f696dafa75c67cbf2068beda605ce6b
JuanNoguera22 <juanf-nogueraa@unilibre.edu.co>
2025-05-26 11:36:36 -0500

    css del login
    se agrego los estilos del css

--- hackaton/src/Login.jsx
+++ hackaton/src/Login.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import React from "react";
-import "./Estilos/Elogin.css";
+// import "./Estilos/Elogin.css";
+import "./Estilos/Login.css";
+
 function Login() {
   return (
     <>
       <div className="login-container">
-        <h1>Login</h1>
-        <div className="container-form">
+        <div className="login-form">
           <p>Id:</p>
-          <input type="text" placeholder="Enter your ID" />
+          <input type="text" placeholder="Enter your ID" className="IdUSer" />
           <p>Password:</p>
-          <input type="password" placeholder="Enter your password" />
+          <input
+            type="password"
+            placeholder="Enter your password"
+            className="PassUser"
+          />
           <br />
-          <button type="button">Entrar</button>
+          <button type="button" className="Bentrar">
+            Entrar
+          </button>
         </div>
       </div>
     </>
