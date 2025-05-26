ae97f6e1407d153a57e95537aa30450a70571624
JuanNoguera22 <juanf-nogueraa@unilibre.edu.co>
2025-05-26 10:56:56 -0500

    react

--- hackaton/src/main.jsx
+++ hackaton/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <App></App>
+    <BrowserRouter>
+      <Routes>
+        {/*  <Route path="/login" element={<Login />} />*/}
+        {/*  <Route path="/formSolicitud" element={<Abogados />} />*/}
+        {/* <Route path="/form" element={<Clientes />} />*/}
+        {/* <Route path="/administrador" element={<Administrador />} />*/}
+        {/* <Route path="/especialidades" element={<Especialidades />} />*/}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
