a5f9273900e7b974a119f0fd07df2245f92f8883
JuanNoguera22 <juanf-nogueraa@unilibre.edu.co>
2025-05-26 11:55:28 -0500

    nav bar
    creado listo para su preparacion con el router

--- hackaton/src/barraNavegacion.jsx
+++ hackaton/src/barraNavegacion.jsx
@@ -1,66 +1,27 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import "./Estilos/styles.css";
 
 function BarraNavegacion() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
     <>
       <header className="header">
         <div className="container">
-          <Link to="/" className="logo">
-            INVENTARIO
-          </Link>
-
-          {/* Navegación de escritorio */}
+          <div className="logo">Gestion quirurgica</div>
           <nav className="desktop-nav">
             <ul>
               <li>
-                <Link to="/solicitud">Solicitud</Link>
+                <span className="navbar-link">Solicitud</span>
               </li>
               <li>
-                <Link to="/devolucion">Devolución</Link>
+                <span className="navbar-link">Devolución</span>
               </li>
               <li>
-                <Link to="/central">Central</Link>
+                <span className="navbar-link">Central</span>
               </li>
             </ul>
           </nav>
-
-          {/* Botón hamburguesa para móvil */}
-          <div className="hamburger" onClick={toggleMenu}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
         </div>
       </header>
-
-      {/* Menú móvil */}
-      <div className={`mobile-menu ${menuOpen ? "active" : ""}`}>
-        <ul>
-          <li>
-            <Link to="/solicitud" onClick={toggleMenu}>
-              Solicitud
-            </Link>
-          </li>
-          <li>
-            <Link to="/devolucion" onClick={toggleMenu}>
-              Devolución
-            </Link>
-          </li>
-          <li>
-            <Link to="/central" onClick={toggleMenu}>
-              Central
-            </Link>
-          </li>
-        </ul>
-      </div>
     </>
   );
 }
