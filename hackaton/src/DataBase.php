97c5a16cfa0834e943470f047873de4c4b619a97
GabrielP1711 <gabriel-padillas@unilibre.edu.co>
2025-05-26 13:59:50 -0500

    conexion de react con mysql

--- hackaton/src/DataBase.php
+++ hackaton/src/DataBase.php
@@ -1,5 +1,17 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Credentials: true');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 class DataBase
 {
     private static $instance = null;
